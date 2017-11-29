4d0682d3d239a4f30eafa6c868b6015193e94075
AlexaPekar <missdreambelle@gmail.com>
2017-11-29 23:16:13 +0100

    Sales special functions are done

--- sales/sales.py
+++ sales/sales.py
@@ -93,16 +93,51 @@ def update(table, id_):
 # return type: string (id)
 # if there are more than one with the lowest price, return the first by descending alphabetical order
 def get_lowest_price_item_id(table):
-
-    # your code
-
-    pass
+    prices = []
+    lowest_ids = []
+    for row in table:
+        prices.append(int(row[2]))
+    for row in table:
+        if int(row[2]) == min(prices):
+            lowest_ids.append(row[0])
+            
+    N = len(lowest_ids)
+    iteration = 1
+    while iteration < N:
+        j = 0
+        while j <= (N-2):
+            if lowest_ids[j] < lowest_ids[j+1]:
+                temp = lowest_ids[j+1]
+                lowest_ids[j+1] = lowest_ids[j]
+                lowest_ids[j] = temp
+                j += 1
+            else:
+                j += 1
+        iteration += 1
+    #lowest_ids.sort(reverse=True)
+    return str(lowest_ids[0])
 
 
 # the question: Which items are sold between two given dates ? (from_date < sale_date < to_date)
 # return type: list of lists (the filtered table)
 def get_items_sold_between(table, month_from, day_from, year_from, month_to, day_to, year_to):
-
-    # your code
-
-    pass
+    date_from = (year_from, month_from, day_from)
+    date_to = (year_to, month_to, day_to)
+    between_dates = []
+    filtered_table = []
+    for i in range(len(table)):
+        actual_month = int(table[i][3])
+        actual_day = int(table[i][4])
+        actual_year = int(table[i][5])
+        actual_date = (actual_year, actual_month, actual_day)
+        if (date_from) < (actual_date) < (date_to):
+            between_dates.append(list(actual_date))
+    for i in range(len(table)):        
+        for date in between_dates:
+            table[i][5] = int(table[i][5])
+            table[i][3] = int(table[i][3])
+            table[i][4] = int(table[i][4])
+            table[i][2] = int(table[i][2])
+            if date[0] == table[i][5] and date[1] == table[i][3] and date[2] == table[i][4]:
+                filtered_table.append(table[i])
+    return filtered_table
