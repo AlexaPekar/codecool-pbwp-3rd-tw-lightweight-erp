a16e2b84cecb1882905eb9308c40cc877bd70f98
GMartin <gabormartin94@gmail.com>
2017-11-29 15:47:15 +0100

    CRM module, HR module are done

--- crm/crm.py
+++ crm/crm.py
@@ -17,83 +17,75 @@ import common
 
 
 def start_module():
-    """
-    Starts this module and displays its menu.
-    User can access default special features from here.
-    User can go back to main menu from here.
-
-    Returns:
-        None
-    """
-
-    # your code
-
-    pass
+    while True:
+        datas = data_manager.get_table_from_file("crm/customers.csv")
+        options = ["Display table", "Add", "Remove", "Update"]
+        ui.print_menu("CRM Menu", options, "Main menu")
+        inputs = ui.get_inputs(["Please, choose an option: "], "")
+        option = inputs[0]
+        if option == "1":
+            show_table(datas)
+        elif option == "2":
+            add(datas)
+        elif option == "3":
+            given_id = ui.get_inputs(["Please enter an ID to remove the line: "], "")
+            remove(datas, given_id)
+        elif option == "4":
+            update_id = ui.get_inputs(["Please enter an ID to update the line: "], "")
+            update(datas, update_id)
+        elif option == "0":
+            break
+        else:
+            ui.print_error_message("There is no such option.")
 
 
 def show_table(table):
-    """
-    Display a table
-
-    Args:
-        table: list of lists to be displayed.
-
-    Returns:
-        None
-    """
-
-    # your code
-
-    pass
+    
+    title_list = ["ID", "Name", "E-mail", "Subscribed"]
+    ui.print_table(table, title_list)
 
 
 def add(table):
-    """
-    Asks user for input and adds it into the table.
 
-    Args:
-        table: table to add new record to
-
-    Returns:
-        Table with a new record
-    """
-
-    # your code
-
+    title_list = ["Name", "E-mail", "Subscribed"]
+    random_id = common.generate_random(table)
+    inputs = ui.get_inputs(title_list, "Please add the items")
+    inputs.insert(0, random_id)
+    table.append(inputs)
+    data_manager.write_table_to_file("crm/customers.csv", table)
     return table
 
 
 def remove(table, id_):
-    """
-    Remove a record with a given id from the table.
 
-    Args:
-        table: table to remove a record from
-        id_ (str): id of a record to be removed
-
-    Returns:
-        Table without specified record.
-    """
-
-    # your code
-
+    ids = common.id_list(table)
+    if id_[0] in ids:
+        for i in range(len(table)):
+            if table[i][0] == id_[0]:
+                table.pop(i)
+                break
+        data_manager.write_table_to_file("crm/customers.csv", table)
+    else:
+        ui.print_error_message("This ID is not in the file!")
     return table
 
 
 def update(table, id_):
-    """
-    Updates specified record in the table. Ask users for new data.
 
-    Args:
-        table: list in which record should be updated
-        id_ (str): id of a record to update
-
-    Returns:
-        table with updated record
-    """
-
-    # your code
-
+    title_list = ["Name", "E-mail", "Subscribed"]
+    new_items = []
+    ids = common.id_list(table)
+    if id_[0] in ids:
+        new_items.append(id_[0])
+        new_elements = ui.get_inputs(title_list, "Please add the items")
+        for item in new_elements:
+            new_items.append(item)
+        for i in range(len(table)):
+            if table[i][0] == id_[0]:
+                table[i] = new_items
+        data_manager.write_table_to_file("crm/customers.csv", table)
+    else:
+        ui.print_error_message("This ID is not in the file!")
     return table
 
 
--- hr/hr.py
+++ hr/hr.py
@@ -16,59 +16,35 @@ import common
 
 
 def start_module():
-    """
-    Starts this module and displays its menu.
-    User can access default special features from here.
-    User can go back to main menu from here.
-
-    Returns:
-        None
-    """
-    datas = data_manager.get_table_from_file("hr/persons.csv")
-    options = ["Display table", "Add", "Remove", "Update"]
-    ui.print_menu("Human resources menu", options, "Main menu")
-    inputs = ui.get_inputs(["Please, choose an option: "], "")
-    option = inputs[0]
-    if option == "1":
-        show_table(datas)
-    elif option == "2":
-        add(datas)
-    elif option == "3":
-        given_id = ui.get_inputs(["Please enter an ID to remove the line: "], "")
-        remove(datas, given_id)
-    elif option == "4":
-        update_id = ui.get_inputs(["Please enter an ID to update the line: "], "")
-        update(datas, update_id)
-    elif option == "0":
-        handle_menu()
-    else:
-        ui.print_error_message("There is no such option.")
+    while True:
+        datas = data_manager.get_table_from_file("hr/persons.csv")
+        options = ["Display table", "Add", "Remove", "Update"]
+        ui.print_menu("Human resources menu", options, "Main menu")
+        inputs = ui.get_inputs(["Please, choose an option: "], "")
+        option = inputs[0]
+        if option == "1":
+            show_table(datas)
+        elif option == "2":
+            add(datas)
+        elif option == "3":
+            given_id = ui.get_inputs(["Please enter an ID to remove the line: "], "")
+            remove(datas, given_id)
+        elif option == "4":
+            update_id = ui.get_inputs(["Please enter an ID to update the line: "], "")
+            update(datas, update_id)
+        elif option == "0":
+            break
+        else:
+            ui.print_error_message("There is no such option.")
 
 
 def show_table(table):
-    """
-    Display a table
 
-    Args:
-        table: list of lists to be displayed.
-
-    Returns:
-        None
-    """
     title_list = ["ID", "Name", "Birth date"]
     ui.print_table(table, title_list)
 
 
 def add(table):
-    """
-    Asks user for input and adds it into the table.
-
-    Args:
-        table: table to add new record to
-
-    Returns:
-        Table with a new record
-    """
 
     title_list = ["Name", "Birth date"]
     random_id = common.generate_random(table)
@@ -80,37 +56,21 @@ def add(table):
 
 
 def remove(table, id_):
-    """
-    Remove a record with a given id from the table.
-
-    Args:
-        table: table to remove a record from
-        id_ (str): id of a record to be removed
-
-    Returns:
-        Table without specified record.
-    """
-
-    for i in range(len(table)):
-        if table[i][0] == id_[0]:
-            table.pop(i)
-            break
-    data_manager.write_table_to_file("hr/persons.csv", table)
+    ids = common.id_list(table)
+    if id_[0] in ids:
+        for i in range(len(table)):
+            if table[i][0] == id_[0]:
+                table.pop(i)
+                break
+        data_manager.write_table_to_file("hr/persons.csv", table)
+    else:
+        ui.print_error_message("This ID is not in the file!")
 
     return table
 
 
 def update(table, id_):
-    """
-    Updates specified record in the table. Ask users for new data.
 
-    Args:
-        table: list in which record should be updated
-        id_ (str): id of a record to update
-
-    Returns:
-        table with updated record
-    """
     title_list = ["Name", "Birth date"]
     new_items = []
     ids = common.id_list(table)
