5d80723697ca37c0e134c0dd9dc91e0ff920dea0
AlexaPekar <missdreambelle@gmail.com>
2017-11-30 11:12:08 +0100

    Display table function is done

--- ui.py
+++ ui.py
@@ -18,10 +18,29 @@ def print_table(table, title_list):
     Returns:
         This function doesn't return anything it only prints to console.
     """
+    """
     seperate = " "
     print(seperate.join(title_list))
     for row in table:
         print(seperate.join(row))
+    """
+    table.insert(0, title_list)
+
+    maxlength = []
+    for column in range(len(table[0])):
+        length = 0
+        for row in range(len(table)):
+            if len(str(table[row][column])) > length:
+                length = len(str(table[row][column]))
+        maxlength.append(length)
+
+    for row_i in range(len(table)):
+        row = table[row_i]
+        print()
+        for cell_j in range(len(row)):
+            cell = row[cell_j]
+            width = maxlength[cell_j]
+            print(" | " + cell.center(width) + " | ", end='')
 
 
 def print_result(result, label):
