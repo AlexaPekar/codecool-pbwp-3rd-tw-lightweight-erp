849a2e04de5a12b9eff0dc6f2c922e6e7e53a672
AlexaPekar <missdreambelle@gmail.com>
2017-11-30 17:26:01 +0100

    Accounting special tasks are implemented

--- accounting/accounting.py
+++ accounting/accounting.py
@@ -6,23 +6,16 @@
 # year: number
 # type: string (in = income, out = outcome)
 # amount: number (dollar)
-
-
-# importing everything you need
 import os
-# User interface module
 import ui
-# data manager module
 import data_manager
-# common module
 import common
 
 
 def start_module():
-
     while True:
         datas = data_manager.get_table_from_file("accounting/items.csv")
-        options = ["Display table", "Add", "Remove", "Update"]
+        options = ["Display table", "Add", "Remove", "Update", "Highest profit year", "Average profit in a given year"]
         ui.print_menu("\nAccounting Menu", options, "Main menu")
         inputs = ui.get_inputs(["Please, choose an option: "], "")
         option = inputs[0]
@@ -36,6 +29,11 @@ def start_module():
         elif option == "4":
             update_id = ui.get_inputs(["Please enter an ID to update the line: "], "")
             update(datas, update_id)
+        elif option == "5":
+            ui.print_result(which_year_max(datas), "is the result of 1st inventory extra function.")
+        elif option == "6":
+            year_list = ui.get_inputs(["Year: "], "Please add the year to get its average profit!")
+            ui.print_result(avg_amount(datas, int(year_list[0])), "is the result of 2nd inventory extra function.")
         elif option == "0":
             break
         else:
@@ -43,13 +41,11 @@ def start_module():
 
 
 def show_table(table):
-
     title_list = ["ID", "Month", "Day", "Year", "Type", "Amount"]
     ui.print_table(table, title_list)
 
 
 def add(table):
-
     title_list = ["Month: ", "Day: ", "Year: ", "Type: ", "Amount: "]
     random_id = common.generate_random(table)
     inputs = ui.get_inputs(title_list, "Please add the items")
@@ -60,7 +56,6 @@ def add(table):
 
 
 def remove(table, id_):
-
     ids = common.id_list(table)
     if id_[0] in ids:
         for i in range(len(table)):
@@ -74,7 +69,6 @@ def remove(table, id_):
 
 
 def update(table, id_):
-
     title_list = ["Month: ", "Day: ", "Year: ", "Type: ", "Amount: "]
     new_items = []
     ids = common.id_list(table)
@@ -99,16 +93,42 @@ def update(table, id_):
 # the question: Which year has the highest profit? (profit=in-out)
 # return the answer (number)
 def which_year_max(table):
+    counter_one = 0
+    counter_two = 0
+    years = []
+    for row in table:
+        years.append(row[3])
+    years = list(set(years))
 
-    # your code
-
-    pass
+    for i in range(len(table)):
+        if table[i][3] == years[0]:
+            if table[i][4] == "in":
+                counter_one += int(table[i][5])
+            elif table[i][4] == "out":
+                counter_one -= int(table[i][5])
+        elif table[i][3] == years[1]:
+            if table[i][4] == "in":
+                counter_two += int(table[i][5])
+            elif table[i][4] == "out":
+                counter_two -= int(table[i][5])
+    if counter_one > counter_two:
+        return int(years[0])
+    else:
+        return int(years[1])
 
 
 # the question: What is the average (per item) profit in a given year? [(profit)/(items count) ]
 # return the answer (number)
 def avg_amount(table, year):
-
-    # your code
-
-    pass
+    counter = 0
+    profit_changes = []
+    for i in range(len(table)):
+        if int(table[i][3]) == year:
+            if table[i][4] == "in":
+                counter += int(table[i][5])
+            elif table[i][4] == "out":
+                counter -= int(table[i][5])
+            profit_changes.append(table[i][4])
+    return counter / len(profit_changes)
+    
+                
